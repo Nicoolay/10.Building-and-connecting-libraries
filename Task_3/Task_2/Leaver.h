3d7362895efddf5c29392b3f045cbe1aa39a762c
Nicoolay <Rozhnov.nv1992@gmail.com>
2024-09-29 10:45:30 +0700

    Correct Cmake file in Task3

--- Task_3/Task_2/Leaver.h
+++ Task_3/Task_2/Leaver.h
@@ -1,14 +1,8 @@
 #pragma once
 #include <string>
 
-
-#ifdef TASK2_EXPORTS
-#define LEAVER_API __declspec(dllexport)
-#else
-#define LEAVER_API __declspec(dllimport)
-#endif
 class  Leaver
 {
 public:
-	LEAVER_API void leave(std::string name);
+	void leave(std::string name);
 };
